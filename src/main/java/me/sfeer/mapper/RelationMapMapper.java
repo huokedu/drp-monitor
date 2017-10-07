b46187142540b5e83297bf4a845429ca2b1c1310
sfeer <sfeer@sina.com>
2017-10-07 11:33:32 +0800

    first commit

--- src/main/java/me/sfeer/mapper/RelationMapMapper.java
+++ src/main/java/me/sfeer/mapper/RelationMapMapper.java
@@ -1,10 +1,7 @@
 package me.sfeer.mapper;
 
 import me.sfeer.domain.RelationMap;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
+import org.apache.ibatis.annotations.*;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -14,6 +11,9 @@ import java.util.List;
 public interface RelationMapMapper {
     @Select("select * from drp_relation_map")
     List<RelationMap> findRelMap();
+
+    @Select("select * from drp_relation_map where id=#{id}")
+    RelationMap findRelMapById(@Param("id") Long id);
 
     @Insert("insert into drp_relation_map(name,`group`,nodes,links,areas) values (#{name},#{group},#{nodes},#{links},#{areas})")
     Long createRelMap(RelationMap map);
