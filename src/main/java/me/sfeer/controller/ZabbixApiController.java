1141760519a4fd8469018a33473df92b022556d6
sfeer <sfeer@sina.com>
2017-10-23 16:58:36 +0800

    添加 zabbix api

--- src/main/java/me/sfeer/controller/ZabbixApiController.java
+++ src/main/java/me/sfeer/controller/ZabbixApiController.java
@@ -1,10 +1,16 @@
 package me.sfeer.controller;
 
+import com.alibaba.fastjson.JSONArray;
 import me.sfeer.service.ZabbixApiService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.Date;
 import java.util.Map;
+
 
 @RestController
 @CrossOrigin
@@ -14,9 +20,41 @@ public class ZabbixApiController {
     @Resource
     private ZabbixApiService zabbixApiService;
 
+    private static final Logger log = LoggerFactory.getLogger(ZabbixApiController.class);
 
+    // 获取主机的最新监控项值
     @GetMapping("/latestdata")
     public Map<String, String> getHostData(@RequestParam("hostid") String id) {
         return zabbixApiService.getHostData(id);
     }
+
+    // 模版列表
+    @GetMapping("/templates")
+    public JSONArray getTemplateList(@RequestParam(value = "name", required = false) String name) {
+        JSONArray s = zabbixApiService.getTemplateList(name);
+        log.info("{}", s.size());
+        return s;
+    }
+
+    // 主机列表
+    @GetMapping("/hosts")
+    public JSONArray getHostList(@RequestParam(value = "name", required = false) String name) {
+        JSONArray s = zabbixApiService.getHostList(name);
+        log.info("{}", s.size());
+        return s;
+    }
+
+    // 历史数据 49170
+    @GetMapping("/history")
+    public JSONArray getHistoryData(@RequestParam("itemid") String id,
+                                    @RequestParam(value = "begin", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") Date begin,
+                                    @RequestParam(value = "end", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") Date end) {
+        // TODO begin, end格式化成时间
+        log.info("time {}", begin.getTime() / 1000L);
+        // TODO 根据查询时间间隔判断查询history还是趋势表
+        JSONArray s = zabbixApiService.getHistoryData(id, begin, end);
+        log.info("{}", s.size());
+        return s;
+    }
+
 }
--- src/main/java/me/sfeer/service/ZabbixApiService.java
+++ src/main/java/me/sfeer/service/ZabbixApiService.java
@@ -1,11 +1,20 @@
 package me.sfeer.service;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import io.github.hengyunabc.zabbix.api.DefaultZabbixApi;
+import io.github.hengyunabc.zabbix.api.Request;
+import io.github.hengyunabc.zabbix.api.RequestBuilder;
+import io.github.hengyunabc.zabbix.api.ZabbixApi;
+import jdk.nashorn.internal.parser.JSONParser;
 import me.sfeer.mapper.ZabbixApiMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,12 +24,76 @@ public class ZabbixApiService {
     @Resource
     private ZabbixApiMapper zabbixApiMapper;
 
+    @Value("${zabbix.url}")
+    private String url;
+
+    @Value("${zabbix.username}")
+    private String username;
+
+    @Value("${zabbix.password}")
+    private String password;
+
+    private ZabbixApi zabbixApi;
+
     private static final Logger log = LoggerFactory.getLogger(ZabbixApiService.class);
+
+    private void initZabbixApi() {
+        zabbixApi = new DefaultZabbixApi(url);
+        zabbixApi.init();
+        zabbixApi.login(username, password);
+    }
 
     public Map<String, String> getHostData(String id) {
         Map<String, String> items = new HashMap<>();
         for (Map<String, Object> item : zabbixApiMapper.selectItemByHostId(id))
             items.put(item.get("key").toString(), item.get("value").toString());
         return items;
+    }
+
+    public JSONArray getTemplateList(String name) {
+        initZabbixApi();
+        RequestBuilder req = RequestBuilder.newBuilder()
+                .method("template.get")
+                .paramEntry("output", new String[]{"templateid", "name"})
+                .paramEntry("selectItems", "count")
+                .paramEntry("selectHosts", new String[]{"hostid", "name", "status"})
+                .paramEntry("sortfield", "name");
+
+        if (name != null && !name.equals(""))
+            req.paramEntry("search", JSONObject.parse("{\"name\":\"" + name + "\"}"));
+
+        return zabbixApi.call(req.build()).getJSONArray("result");
+    }
+
+    public JSONArray getHostList(String name) {
+        initZabbixApi();
+        RequestBuilder req = RequestBuilder.newBuilder()
+                .method("host.get")
+                .paramEntry("output", new String[]{"hostid", "name", "status"})
+                .paramEntry("selectInterfaces", new String[]{"ip", "port"})
+                .paramEntry("selectItems", "count")
+                .paramEntry("selectParentTemplates", new String[]{"templateid", "name"})
+                .paramEntry("sortfield", "name");
+
+        if (name != null && !name.equals(""))
+            req.paramEntry("search", JSONObject.parse("{\"name\":\"" + name + "\"}"));
+
+        return zabbixApi.call(req.build()).getJSONArray("result");
+    }
+
+    public JSONArray getHistoryData(String id, Date begin, Date end) {
+        initZabbixApi();
+        RequestBuilder req = RequestBuilder.newBuilder()
+                .method("history.get")
+                .paramEntry("itemids", id)
+                .paramEntry("history", 0)
+                .paramEntry("limit", 100)
+                .paramEntry("sortorder", "DESC")
+                .paramEntry("sortfield", "clock");
+
+        if(begin != null)
+            req.paramEntry("time_from", begin.getTime() / 1000L);
+
+        return zabbixApi.call(req.build()).getJSONArray("result");
     }
 }
